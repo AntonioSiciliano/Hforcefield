4c07a3d6fee14330f935880a8dce2c72febab18e
AntonioSiciliano <antoniosiciliano@yahoo.it>
2021-06-09 18:54:19 +0200

    Now it is working

--- Modules/.ipynb_checkpoints/Calculator-checkpoint.py
+++ Modules/.ipynb_checkpoints/Calculator-checkpoint.py
@@ -13,11 +13,11 @@ class ToyModelCalculator(calc.Calculator):
 
     def __init__(self,  *args, **kwargs):
         """
-        Computes the potential and forces for an H2 molecule:
+        Computes the potential and forces for an H atom:
         
-            V(r_1, r_2) = H2_shift + H2_D * {1 - exp[- H2_a * (r - H2_re)]}^2 + E * (x_1 - x_2),
+            V(r) = H2_shift + H2_D * {1 - exp[- H2_a * (r - H2_re)]}^2 + E * x,
         
-            where r = |r_1 - r_2|.
+            where r = |(x,y,z)|.
         """
         calc.Calculator.__init__(self, *args, **kwargs)
         
@@ -70,29 +70,27 @@ class ToyModelCalculator(calc.Calculator):
         calc.Calculator.calculate(self, atoms, *args, **kwargs)
         
         # Energy and force in HARTREE and HARTREE/BOHR
-        energy, force = 0., np.zeros((2, 3), dtype = np.double)
+        energy, force = 0., np.zeros((1,3), dtype = np.double)
 
         # Position in ANGSTROM converted in BOHR, np.array with shape = (2, 3)
         coords = atoms.get_positions() * units.A_TO_BOHR
-
+        
         # Get the relative coordinate
-        rel_coord =  (coords[0,:] - coords[1,:])
+        rel_coord =  np.sqrt(coords[0,:]**2)
         
         # Get the radial distance
-        r         = np.sqrt(rel_coord.dot(rel_coord))
+        r         = np.sqrt(np.sum(rel_coord**2))
         
         # Get the energy in HARTREE subtrating the minimum of the Morse + crystal field potential
-        energy = self.H2_shift + self.H2_D * (1. - np.exp(-self.H2_a * (r - self.H2_re)))**2 + self.E * (coords[0,0] - coords[1,0]) - self.minimum()
+        energy = self.H2_shift + self.H2_D * (1. - np.exp(-self.H2_a * (r - self.H2_re)))**2 + self.E * coords[0,0] - self.minimum()
         
         # Derivative with respect the radial distance
         diff_V_r = 2. * self.H2_a * self.H2_D * (1. - np.exp(-self.H2_a * (r - self.H2_re))) * np.exp(-self.H2_a * (r - self.H2_re))
         
         # Get the forces for the first particle in HARTREE /BOHR
-        force[0,:]  = - diff_V_r * (coords[0,:] - coords[1,:]) /r
+        force[0,:]  = - diff_V_r * coords /r
         force[0,0] += - self.E
-        
-        # Get the forces for the second particle in HARTREE /BOHR
-        force[1,:] = - force[0,:]
+
         
         # CONVERT from HARTREE, HARTREE /BOHR in -> eV, eV /ANGSTROM
         self.results = {"energy": energy * 2. * units.RY_TO_EV, "forces": force * 2. * units.RY_TO_EV /units.BOHR_TO_ANGSTROM}
